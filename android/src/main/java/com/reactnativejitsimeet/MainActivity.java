e1e1ea1d06baaf4aa26d769b7fc373d814580fa3
Fabrizio Corpora <fabrizio.corpora@gmail.com>
2023-05-30 19:51:59 +0200

    get example jitsi mainactivity

--- android/src/main/java/com/reactnativejitsimeet/MainActivity.java
+++ android/src/main/java/com/reactnativejitsimeet/MainActivity.java
@@ -1,38 +1,87 @@
 package com.reactnativejitsimeet;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
-import org.jitsi.meet.sdk.JitsiMeetView;
+import androidx.fragment.app.FragmentActivity;
+import com.facebook.react.modules.core.PermissionListener;
+import com.reactnativejitsimeet.ExJitsiMeetView;
 
-public class MainActivity extends AppCompatActivity {
-    private JitsiMeetView jitsiMeetView;
+import org.jitsi.meet.sdk.JitsiMeetActivityDelegate;
+import org.jitsi.meet.sdk.JitsiMeetActivityInterface;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+public class MainActivity extends FragmentActivity implements JitsiMeetActivityInterface {
+    private ExJitsiMeetView view;
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        JitsiMeetActivityDelegate.onActivityResult(
+                this, requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        JitsiMeetActivityDelegate.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Inizializza la vista Jitsi Meet
-        jitsiMeetView = new ExJitsiMeetView(this);
-
-        // Configura le opzioni della conferenza
+        view = new ExJitsiMeetView(this);
         JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                .setRoom("https://meet.jit.si/your-room-name")
+                .setRoom("https://meet.jit.si/test123")
                 .build();
+        view.join(options);
 
-        // Carica le opzioni della conferenza nella vista Jitsi Meet
-        jitsiMeetView.join(options);
-
-        // Aggiungi la vista Jitsi Meet alla tua gerarchia di visualizzazione
-        setContentView(jitsiMeetView);
+        setContentView(view);
     }
 
     @Override
     protected void onDestroy() {
-        // Esegui la pulizia quando l'attività viene distrutta
-        jitsiMeetView.dispose();
-        jitsiMeetView = null;
-
         super.onDestroy();
+
+        view.dispose();
+        view = null;
+
+        JitsiMeetActivityDelegate.onHostDestroy(this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        JitsiMeetActivityDelegate.onNewIntent(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            final int requestCode,
+            final String[] permissions,
+            final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        JitsiMeetActivityDelegate.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        JitsiMeetActivityDelegate.onHostResume(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        JitsiMeetActivityDelegate.onHostPause(this);
+    }
+
+    @Override
+    public void requestPermissions(String[] strings, int i, PermissionListener permissionListener) {
+
     }
 }
